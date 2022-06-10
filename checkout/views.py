b5aeb29aa9bf1dfb4eb68c1733aab2b9d8e79ee5
YoannaLa <joannaelazarek@yahoo.co.uk>
2022-06-10 19:39:58 +0000

    adding stripe payment

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51L97OiCQmncmYtAI6SLhmDxcIz5WtL6dFY8FwdNdabzRSBpGJDqmRRVNCNp1KoiWNB62mkYMb6iqf6F17AqHKhHe0040jj77gm',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
