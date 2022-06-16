38dc9a1fdb5551630f57fbb7388eed6b28b549b6
YoannaLa <joannaelazarek@yahoo.co.uk>
2022-06-16 10:01:10 +0000

    part 2 of deployment

--- boutique_crossfit/settings.py
+++ boutique_crossfit/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-tgt&0)7^nwxf-uo!35@6o&jh7yic++6@c=&t)6cxggo-!qjz*d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['crossgit-gear.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,13 +122,17 @@ WSGI_APPLICATION = 'boutique_crossfit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
