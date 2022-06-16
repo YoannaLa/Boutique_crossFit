5059154155c04410216162737fb1bd49b2e7516e
YoannaLa <joannaelazarek@yahoo.co.uk>
2022-06-16 09:05:30 +0000

    first part of deloyment recheck

--- boutique_crossfit/settings.py
+++ boutique_crossfit/settings.py
@@ -123,11 +123,13 @@ WSGI_APPLICATION = 'boutique_crossfit.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-  'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
