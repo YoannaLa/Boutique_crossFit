8e2ed58efba10d461c7bc8f1b735b3e1e8ee3066
YoannaLa <joannaelazarek@yahoo.co.uk>
2022-08-21 14:43:47 +0000

    adding new eamil to setting.py

--- boutique_crossfit/settings.py
+++ boutique_crossfit/settings.py
@@ -227,7 +227,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'crossfit_gear@example.com'
+    DEFAULT_FROM_EMAIL = 'yoanna.lazarek@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
